acdf81f798c1c132e111cb7e8c51627ba06af797
Jess Alejo <tbki.jea@gmail.com>
2023-04-21 14:04:17 +0800

    Fix image path of brands

--- brand-logos.js
+++ brand-logos.js
@@ -1,154 +1,154 @@
 const brandLogos = [
   {
     name: "adidas",
-    image: "/images/logos/adidas.png",
+    image: "images/logos/adidas.png",
   },
   {
     name: "apple",
-    image: "/images/logos/apple.png",
+    image: "images/logos/apple.png",
   },
   {
     name: "audi",
-    image: "/images/logos/audi.png",
+    image: "images/logos/audi.png",
   },
   {
     name: "caltex",
-    image: "/images/logos/caltex.png",
+    image: "images/logos/caltex.png",
   },
   {
     name: "cebu pacific",
-    image: "/images/logos/cebu pacific.jpeg",
+    image: "images/logos/cebu pacific.jpeg",
   },
   {
     name: "chanel",
-    image: "/images/logos/chanel.png",
+    image: "images/logos/chanel.png",
   },
   {
     name: "chowking",
-    image: "/images/logos/chowking.jpeg",
+    image: "images/logos/chowking.jpeg",
   },
   {
     name: "dunkin donuts",
-    image: "/images/logos/dunkin donuts.png",
+    image: "images/logos/dunkin donuts.png",
   },
   {
     name: "ferrari",
-    image: "/images/logos/ferrari.png",
+    image: "images/logos/ferrari.png",
   },
   {
     name: "fruit of the loom",
-    image: "/images/logos/fruit of the loom.png",
+    image: "images/logos/fruit of the loom.png",
   },
   {
     name: "globe",
-    image: "/images/logos/globe.png",
+    image: "images/logos/globe.png",
   },
   {
     name: "jollibee",
-    image: "/images/logos/jollibee.jpeg",
+    image: "images/logos/jollibee.jpeg",
   },
   {
     name: "kodak",
-    image: "/images/logos/kodak.png",
+    image: "images/logos/kodak.png",
   },
   {
     name: "lacoste",
-    image: "/images/logos/lacoste.jpeg",
+    image: "images/logos/lacoste.jpeg",
   },
   {
     name: "landbank",
-    image: "/images/logos/landbank.png",
+    image: "images/logos/landbank.png",
   },
   {
     name: "mastercard",
-    image: "/images/logos/mastercard.png",
+    image: "images/logos/mastercard.png",
   },
   {
     name: "maynilad",
-    image: "/images/logos/maynilad.jpeg",
+    image: "images/logos/maynilad.jpeg",
   },
   {
     name: "meralco",
-    image: "/images/logos/meralco.png",
+    image: "images/logos/meralco.png",
   },
   {
     name: "mercedes-benz",
-    image: "/images/logos/mercedes-benz.jpeg",
+    image: "images/logos/mercedes-benz.jpeg",
   },
   {
     name: "metrobank",
-    image: "/images/logos/metrobank.png",
+    image: "images/logos/metrobank.png",
   },
   {
     name: "netflix",
-    image: "/images/logos/netflix.png",
+    image: "images/logos/netflix.png",
   },
   {
     name: "nike",
-    image: "/images/logos/nike.png",
+    image: "images/logos/nike.png",
   },
   {
     name: "paypal",
-    image: "/images/logos/paypal.png",
+    image: "images/logos/paypal.png",
   },
   {
     name: "pepsi",
-    image: "/images/logos/pepsi.jpeg",
+    image: "images/logos/pepsi.jpeg",
   },
   {
     name: "petron",
-    image: "/images/logos/petron.png",
+    image: "images/logos/petron.png",
   },
   {
     name: "playstation",
-    image: "/images/logos/playstation.png",
+    image: "images/logos/playstation.png",
   },
   {
     name: "puma",
-    image: "/images/logos/puma.png",
+    image: "images/logos/puma.png",
   },
   {
     name: "robinsons",
-    image: "/images/logos/robinsons.png",
+    image: "images/logos/robinsons.png",
   },
   {
     name: "rolex",
-    image: "/images/logos/rolex.png",
+    image: "images/logos/rolex.png",
   },
   {
     name: "san miguel",
-    image: "/images/logos/san miguel.png",
+    image: "images/logos/san miguel.png",
   },
   {
     name: "shell",
-    image: "/images/logos/shell.png",
+    image: "images/logos/shell.png",
   },
   {
     name: "spotify",
-    image: "/images/logos/spotify.png",
+    image: "images/logos/spotify.png",
   },
   {
     name: "starbucks",
-    image: "/images/logos/starbucks.png",
+    image: "images/logos/starbucks.png",
   },
   {
     name: "tesda",
-    image: "/images/logos/tesda.png",
+    image: "images/logos/tesda.png",
   },
   {
     name: "twitter",
-    image: "/images/logos/twitter.png",
+    image: "images/logos/twitter.png",
   },
   {
     name: "unilever",
-    image: "/images/logos/unilever.png",
+    image: "images/logos/unilever.png",
   },
   {
     name: "volkswagen",
-    image: "/images/logos/volkswagen.png",
+    image: "images/logos/volkswagen.png",
   },
   {
     name: "windows",
-    image: "/images/logos/windows.png",
+    image: "images/logos/windows.png",
   },
 ]
